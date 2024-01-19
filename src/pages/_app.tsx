a32fc46d72269bd6ae7ed7c3f5a57e44c8252aec
Ramon Fontana <30450181+RamonFontana@users.noreply.github.com>
2024-01-19 10:24:06 -0300

    create home

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,19 @@ import { globalStyles } from '../styles/global'
 
 globalStyles()
 
+import logoImg from '../assets/logo.svg'
+import { Container, Header } from '../styles/pages/app'
+
+import Image from 'next/image'
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+      
+      <Component {...pageProps} />
+    </Container>
+  )
 }
